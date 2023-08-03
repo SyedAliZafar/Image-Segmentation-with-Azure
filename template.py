ffe3e7f855e63ca9ea6069bfdbc69d100e4c6a45
SyedAliZafar <syedalizafar0@gmail.com>
2023-08-03 11:17:22 +0200

    internal files

--- template.py
+++ template.py
@@ -15,7 +15,7 @@ list_of_files = [
         ".github/workflows/.gitkeep",
         f"src/{project_name}/__init__.py", 
         f"src/{project_name}/components/__init__.py", 
-        f"src/{project_name}/utils__init__.py",  
+        f"src/{project_name}/utils/__init__.py",  
         f"src/{project_name}/config/__init__.py",
         f"src/{project_name}/config/configuration.py",
         f"src/{project_name}/pipeline/__init__.py",
